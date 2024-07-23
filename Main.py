6c0a0fc01853dbe607e031f0ea5c8511eb260f2e
Adelita Martinez <amartinez1013@cnm.edu>
2024-07-23 09:00:48 -0600

    Added imports

--- Main.py
+++ Main.py
@@ -3,3 +3,9 @@
 # Email: amartinez1013@cnm.edu
 # Purpose: Demonstrate how to use a GUI and read from a database
 # Python Version: 3.12.3
+
+import tkinter as tk
+from tkinter import messagebox
+from GeoPoint import GeoPoint
+import sqlite3
+
