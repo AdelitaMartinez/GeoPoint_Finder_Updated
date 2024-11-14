5ef1104876c908426e6a90a506f11253b6f36581
Adelita Martinez <amartinez1013@cnm.edu>
2024-11-14 09:45:40 -0700

    Corrected email

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # main.py
 # Programmer: Adelita Martinez
-# Email: amartinez1013@cnm.edu
+# Email: 94martinez.adelita@gmail.com
 # Purpose: Demonstrate how to use a GUI and read points from a database
 # Python Version: 3.12.3
 
